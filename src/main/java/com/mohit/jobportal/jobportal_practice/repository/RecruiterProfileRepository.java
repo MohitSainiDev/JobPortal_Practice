f3da26256c9df68a38528f6bcb403de81d41cf2c
MohitSainiDev <mht4943@gmail.com>
2024-10-20 23:05:35 +0530

    Added jobSeeker and Recruiter Profiles

---
+++ src/main/java/com/mohit/jobportal/jobportal_practice/repository/RecruiterProfileRepository.java
@@ -0,0 +1,9 @@
+package com.mohit.jobportal.jobportal_practice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mohit.jobportal.jobportal_practice.entity.RecruiterProfile;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+
+}
--- src/main/java/com/mohit/jobportal/jobportal_practice/service/UsersService.java
+++ src/main/java/com/mohit/jobportal/jobportal_practice/service/UsersService.java
@@ -6,22 +6,36 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mohit.jobportal.jobportal_practice.entity.JobSeekerProfile;
+import com.mohit.jobportal.jobportal_practice.entity.RecruiterProfile;
 import com.mohit.jobportal.jobportal_practice.entity.Users;
+import com.mohit.jobportal.jobportal_practice.repository.JobSeekerProfileRepository;
+import com.mohit.jobportal.jobportal_practice.repository.RecruiterProfileRepository;
 import com.mohit.jobportal.jobportal_practice.repository.UsersRepository;
 
 @Service
 public class UsersService {
 
 	@Autowired
-	UsersRepository userRepository;
+	UsersRepository usersRepository;
+	@Autowired
+	JobSeekerProfileRepository jobSeekerProfileRepository;
+	@Autowired
+	RecruiterProfileRepository recruiterProfileRepository;
 
 	public Users addNew(Users users) {
 		users.setActive(true);
 		users.setRegistrationDate(new Date(System.currentTimeMillis()));
-		return userRepository.save(users);
+		Users savedUser = usersRepository.save(users);
+		int userTypeId = users.getUserTypeId().getUserTypeId();
+		if (userTypeId == 1) {
+			recruiterProfileRepository.save(new RecruiterProfile(users));
+		} else
+			jobSeekerProfileRepository.save(new JobSeekerProfile(users));
+		return savedUser;
 	}
 
 	public Optional<Users> getUserByEmail(String email) {
-		return userRepository.findByEmail(email);
+		return usersRepository.findByEmail(email);
 	}
 }
